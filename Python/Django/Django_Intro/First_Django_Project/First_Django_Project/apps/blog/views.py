112c21ddec420127c139177ddc3266c0fd96d060
MorningReference <aricjeon@gmail.com>
2020-05-11 14:44:51 -0700

    Finished Users with template assignment

--- Python/Django/Django_Intro/First_Django_Project/First_Django_Project/apps/blog/views.py
+++ Python/Django/Django_Intro/First_Django_Project/First_Django_Project/apps/blog/views.py
@@ -10,10 +10,10 @@ def create(request):
     return redirect("/")
 
 def show(request, number):
-    return HttpResponse(f"placeholder to display blog number: {number}")
+    return HttpResponse(f"placeholder to display blog number: {{number}}")
 
 def edit(request, number):
-    return HttpResponse(f"placeholder to edit blog {number}")
+    return HttpResponse(f"placeholder to edit blog {{number}}")
 
 def destroy(request, number):
     return redirect("/")
