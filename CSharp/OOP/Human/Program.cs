74c325afa03c826ad076b3da0e02197c38395eef
MorningReference <aricjeon@gmail.com>
2020-06-02 13:14:58 -0700

    Human assignment completed;

---
+++ CSharp/OOP/Human/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human Bob = new Human("Bob");
+            Human George = new Human("George", 10,10,10,200);
+            Bob.Attack(George);
+            Console.WriteLine(George.Health);
+        }
+    }
+}
