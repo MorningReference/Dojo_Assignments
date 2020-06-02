b612daf6a26a6c48a578109e6b80813693e40305
MorningReference <aricjeon@gmail.com>
2020-06-01 20:38:47 -0700

    Boxing/UnBoxing Assignment completed

---
+++ CSharp/CSharp/BoxingAndUnboxing/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingAndUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> boxedData = new List<object>();
+            boxedData.Add(7);
+            boxedData.Add(28);
+            boxedData.Add(-1);
+            boxedData.Add(true);
+            boxedData.Add("chair");
+            int sum = 0;
+            foreach (var entry in boxedData)
+            {
+                Console.WriteLine(entry);
+                if(entry is int)
+                {
+                    sum += (int)entry;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
