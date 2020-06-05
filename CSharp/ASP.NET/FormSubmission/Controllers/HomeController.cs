65368b89d8bacb4b30b517ca478b13e7b28bd9f5
MorningReference <aricjeon@gmail.com>
2020-06-05 00:11:26 -0700

    Validating Form Submission assignment complete

---
+++ CSharp/ASP.NET/FormSubmission/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FormSubmission.Models;
+
+namespace FormSubmission.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            User user = new User();
+            return View("Index", user);
+        }
+        [HttpPost("create")]
+        public IActionResult CreateUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                var newUser = user;
+                return View("Success");
+            }
+            else
+            {
+                return View("Index", user);
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CSharp/ASP.NET/FormSubmission/Models/User.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace FormSubmission.Models
+{
+    public class User
+    {
+        [Required]
+        [MinLength(4)]
+        [Display(Name = "First Name:")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [Display(Name = "Last Name:")]
+
+        public string LastName { get; set; }
+
+        [Required]
+        [Range(0, int.MaxValue)]
+        [Display(Name = "Age:")]
+
+        public int Age { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address:")]
+
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [MinLength(8)]
+        [Display(Name = "Password:")]
+
+        public string Password { get; set; }
+    }
+}
