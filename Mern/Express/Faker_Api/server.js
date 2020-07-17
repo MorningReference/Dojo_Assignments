7daf57eed208421d36140d47d4ac627252661ccd
MorningReference <aricjeon@gmail.com>
2020-07-16 22:16:57 -0700

    Completed Faker API assignment

---
+++ Mern/Express/Faker_Api/server.js
@@ -0,0 +1,44 @@
+const faker = require('faker');
+const express = require('express');
+const app = express();
+const port = 8000;
+
+class User {
+    constructor() {
+        this._id = faker.random.number();
+        this.firstName = faker.name.firstName();
+        this.lastName = faker.name.lastName();
+        this.phoneNumber = faker.phone.phoneNumber();
+        this.email = faker.internet.email();
+        this.password = faker.internet.password();
+    }
+}
+class Company {
+    constructor() {
+        this._id = faker.random.number();
+        this.firstName = faker.company.companyName();
+        this.address = {
+            street: faker.address.streetAddress(),
+            city: faker.address.city(),
+            state: faker.address.state(),
+            zipCode: faker.address.zipCode(),
+            country: faker.address.country(),
+        };
+    }
+}
+
+app.use(express.json());
+
+app.get('/api/users/new', (req, res) => {
+    res.json(new User());
+});
+
+app.get('/api/companies/new', (req, res) => {
+    res.json(new Company());
+});
+
+app.get('/api/user/company', (req, res) => {
+    res.json([new User(), new Company()]);
+});
+
+app.listen(port, () => console.log('Listening on port: ', port));
